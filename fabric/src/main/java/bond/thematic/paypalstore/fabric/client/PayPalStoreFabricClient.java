9251074a7eda592920e98d0385d45ed7dc35fdfe
alexw <alex.j.witkowski@gmail.com>
2025-12-26 15:20:01 -0500

    fix: update naming

---
+++ fabric/src/main/java/bond/thematic/paypalstore/fabric/client/PayPalStoreFabricClient.java
@@ -0,0 +1,11 @@
+package bond.thematic.paypalstore.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class PayPalStoreFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as
+        // rendering.
+    }
+}
